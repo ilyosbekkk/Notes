26e70f941770abf3ce34788ed6cd1933a626b549
Ilyosbek Ibrokhimov <46449085+ilyosbekibrohimov@users.noreply.github.com>
2020-10-03 23:28:48 +0900

    enable/disable edit mode added

--- app/src/main/java/com/example/notes/NewActivity.java
+++ app/src/main/java/com/example/notes/NewActivity.java
@@ -9,23 +9,31 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.notes.models.Note;
 
-public class NewActivity extends AppCompatActivity
-        implements View.OnTouchListener, GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+public class NewActivity extends AppCompatActivity implements View.OnTouchListener, GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener, View.OnClickListener {
 
+    //region  constants
+    private static final int EDIT_MODE_ENABLED = 1;
+    private static final int EDIT_MODE_DISABLED = 0;
+    private static final String TAG = "NewActivity";
+    //endregion
     //region UI componenets
     private LinedEditText mLinedEditText;
     private EditText mEditText;
     private TextView mViewTitle;
+    private RelativeLayout mCheckContainer, mBackArrowConatiner;
+    private ImageButton mCheck, mBackArrow;
     //endregion
     //region vars
     Note mInitialNote;
     private boolean mIsNewNote;
-    private static final String TAG = "NewActivity";
     private GestureDetector mGestureDetector;
+    private int mMode;
 
 
     //endregion
@@ -37,16 +45,53 @@ public class NewActivity extends AppCompatActivity
         mLinedEditText = findViewById(R.id.note_text);
         mEditText = findViewById(R.id.note_edit_title);
         mViewTitle = findViewById(R.id.note_text_title);
+        mCheckContainer = findViewById(R.id.check_container);
+        mBackArrowConatiner = findViewById(R.id.back_arrow_container);
+        mCheck = findViewById(R.id.check_arrow);
+        mBackArrow = findViewById(R.id.toolbar_back_arrow);
 
         if (getIncomingIntent()) {
             //edit mode
+            enableEditMode();
             setNewNoteProperties();
         } else {
             //view mode
             setNoteProperties();
         }
+
+
         setListener();
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mMode == EDIT_MODE_ENABLED) {
+            onClick(mCheck);
+        } else
+            super.onBackPressed();
+    }
+
+    //endregion
+    //region enable/disable edit mode
+    private void enableEditMode() {
+        mBackArrowConatiner.setVisibility(View.GONE);
+        mCheckContainer.setVisibility(View.VISIBLE);
+
+        mViewTitle.setVisibility(View.GONE);
+        mEditText.setVisibility(View.VISIBLE);
+
+        mMode = EDIT_MODE_ENABLED;
+    }
+
+    private void disableEditMode() {
+        mBackArrowConatiner.setVisibility(View.VISIBLE);
+        mCheckContainer.setVisibility(View.GONE);
+
+        mViewTitle.setVisibility(View.VISIBLE);
+        mEditText.setVisibility(View.GONE);
+
+        mMode = EDIT_MODE_DISABLED;
     }
 
     //endregion
@@ -57,8 +102,10 @@ public class NewActivity extends AppCompatActivity
             assert mInitialNote != null;
             Log.d(TAG, "getIncomingIntent: " + mInitialNote.toString());
             mIsNewNote = false;
+            mMode = EDIT_MODE_DISABLED;
             return false;
         } else {
+            mMode = EDIT_MODE_ENABLED;
             mIsNewNote = true;
             return true;
         }
@@ -66,6 +113,7 @@ public class NewActivity extends AppCompatActivity
 
     //endregion
     //region set New/Not New Note Properties
+    @SuppressLint("SetTextI18n")
     private void setNewNoteProperties() {
         mViewTitle.setText("Note Title");
         mEditText.setText("Note Title");
@@ -81,20 +129,25 @@ public class NewActivity extends AppCompatActivity
 
     //endregion
     //region listener(s)
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         return mGestureDetector.onTouchEvent(event);
     }
 
-
+    @SuppressLint("ClickableViewAccessibility")
     private void setListener() {
         mLinedEditText.setOnTouchListener(this);
+        mViewTitle.setOnClickListener(this);
+        mCheck.setOnClickListener(this);
         mGestureDetector = new GestureDetector(this, this);
+
 
     }
 
     @Override
     public boolean onDown(MotionEvent e) {
+
         return false;
     }
 
@@ -105,17 +158,18 @@ public class NewActivity extends AppCompatActivity
 
     @Override
     public boolean onSingleTapUp(MotionEvent e) {
+
         return false;
     }
 
     @Override
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+
         return false;
     }
 
     @Override
     public void onLongPress(MotionEvent e) {
-
     }
 
     @Override
@@ -131,12 +185,28 @@ public class NewActivity extends AppCompatActivity
     @Override
     public boolean onDoubleTap(MotionEvent e) {
         Log.d(TAG, "Double Pressed...");
+        enableEditMode();
         return false;
     }
 
     @Override
     public boolean onDoubleTapEvent(MotionEvent e) {
         return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.check_arrow:
+                disableEditMode();
+                break;
+            case R.id.note_text_title:
+                enableEditMode();
+                mEditText.requestFocus();
+                mEditText.setSelection(mEditText.length());
+
+
+        }
     }
     //endregion
 
